a27ca19514dcb4cc02508a5dacbb4e6343e9d353
Pedrohme <pedrohmen45@gmail.com>
2022-02-01 20:01:16 -0300

    feat: servidor tcp basico (teste)

--- servidor/src/app.ts
+++ servidor/src/app.ts
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+import * as net from 'net';
+
+const server = net.createServer((socket) => {
+    // 'connection' listener.
+    console.log('client connected');
+    socket.on('end', () => {
+        console.log('client disconnected');
+    });
+    socket.write('hello\r\n');
+    socket.pipe(socket);
+});
+server.on('error', (err) => {
+    throw err;
+});
+server.listen(8124, () => {
+    console.log('server bound');
+});
